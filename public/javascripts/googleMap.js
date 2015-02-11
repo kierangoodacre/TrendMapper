95cebc1e85fcecbf5efc9939c40f4807ad8a4e2d
guspowell <augustuspowell@me.com>
2015-02-11 10:30:20 +0000

    indentation

--- public/javascripts/googleMap.js
+++ public/javascripts/googleMap.js
@@ -1,9 +1,9 @@
-
 var map;
+
 function initialize() {
-map = new google.maps.Map(document.getElementById('map-canvas'), {
-zoom: 11,
-center: {lat: 51.51, lng: - 0.10}
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+  zoom: 11,
+  center: {lat: 51.51, lng: - 0.10}
 });
 
 // Load GeoJSON.
@@ -13,32 +13,32 @@ map.data.loadGeoJson('https://cdn.rawgit.com/jjlakin/compound-cities/master/grea
 // Color each letter gray. Change the color when the isColorful property
 // is set to true.
 map.data.setStyle(function(feature) {
-var color = 'red';
-if (feature.getProperty('isColorful')) {
-  color = feature.getProperty('color');
-}
-return /** @type {google.maps.Data.StyleOptions} */({
-  fillColor: color,
-  strokeColor: color,
-  strokeWeight: 2
-});
+  var color = 'red';
+  if (feature.getProperty('isColorful')) {
+    color = feature.getProperty('color');
+  }
+  return /** @type {google.maps.Data.StyleOptions} */({
+    fillColor: color,
+    strokeColor: color,
+    strokeWeight: 2
+  });
 });
 
 // When the user clicks, set 'isColorful', changing the color of the letters.
 map.data.addListener('click', function(event) {
-event.feature.setProperty('isColorful', true);
+  event.feature.setProperty('isColorful', true);
 });
 
 // When the user hovers, tempt them to click by outlining the letters.
 // Call revertStyle() to remove all overrides. This will use the style rules
 // defined in the function passed to setStyle()
 map.data.addListener('mouseover', function(event) {
-map.data.revertStyle();
-map.data.overrideStyle(event.feature, {strokeWeight: 8});
+  map.data.revertStyle();
+  map.data.overrideStyle(event.feature, {strokeWeight: 8});
 });
 
 map.data.addListener('mouseout', function(event) {
-map.data.revertStyle();
+  map.data.revertStyle();
 });
 // [END snippet]
 }
