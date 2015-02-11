e48b988e4a06355396b4b0c78470906c7653b66f
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-02-11 08:55:19 +0000

    created array of London Boroughs

--- data/spec/app_spec.rb
+++ data/spec/app_spec.rb
@@ -1,11 +1,15 @@
-require 'app'
+require 'directory'
 
 describe 'Data pulling' do
 
-  let(:data){Data.new}
+  let(:data){Directory.new}
 
   it 'Will contain an array of London boroughs' do
-    expect(data.borough_array.length).to eq 32
+    expect(data.borough_array.length).to eq 33
   end
+
+  it 'Will contain an array of hipster keywords'do
+  	expect(data.hipster_keyword_array.length).to eq 47
+	end
 
 end
