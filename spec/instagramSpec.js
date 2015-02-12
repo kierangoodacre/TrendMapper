022fe4970a64fc6a810f68bb9fbe45c8a378e8d1
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-02-12 10:18:56 +0000

    created our server test

--- spec/instagramSpec.js
+++ spec/instagramSpec.js
@@ -1,13 +1,27 @@
-var api = require('hippie');
+var instagram = require('hippie');
+var server = require('hippie');
 
-api()
+instagram()
   .header('User-Agent', 'hippie')
   .json()
-  .get('https://api.github.com/users/bebbs')
+  .get('https://api.instagram.com/v1/tags/hackney/media/recent?client_id=89cc7d4644154c718cc5fb612e5da3cb')
   .expectStatus(200)
   .end(function(err, res, body) {
     if (err) {
       throw err 
+    } else {
+      console.log('Test passed');
+    }
+  });
+
+server()
+  .header('User-Agent', 'hippie')
+  .json()
+  .get('http://localhost:3000/data/images')
+  .expectStatus(200)
+  .end(function(err, res, body){
+    if (err) {
+      console.log('NOT PASSED') throw err
     } else {
       console.log('Test passed');
     }
